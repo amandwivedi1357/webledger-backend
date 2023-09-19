fe64d083eb2e2b04a202519dfcf17f9a339e34a9
Aman Dwivedi <amandwivedi71124.com>
2023-09-19 14:20:56 +0530

    added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
     
   });
 
-app.listen(process.env.port,async()=>{
+app.listen(process.env.PORT,async()=>{
     try {
         await connection;
         console.log("connected to DB");
